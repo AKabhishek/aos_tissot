5afb1c4f0cd2c20ead8d2ee37e69def405008007
AKabhishek <abhishekkaushik.star@gmail.com>
2018-04-02 10:08:35 +0000

    tissot: add aos ota config
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- prop.mk
+++ prop.mk
@@ -2,3 +2,9 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.aos.maintainer=Abhishek AK
+
+# OTA Configs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ota.romname=Atomic-OS \
+    ro.ota.version=$(shell date +"%Y%m%d") \
+    ro.ota.manifest=https://raw.githubusercontent.com/TeamDarkness-Devices/ota_devices/master/tissot.xml
