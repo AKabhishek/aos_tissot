AKabhishek/aos_mido

Bring atomic os
Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

aos.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Atomic-OS stuff.
+$(call inherit-product, vendor/aos/config/common.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := aos_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

AKabhishek/aos_tissot

aos.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Atomic-OS stuff.
+$(call inherit-product, vendor/aos/config/common.mk)
 
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := aos_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi